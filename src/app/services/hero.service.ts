2a0bb42042184f18f5e9fd31c35a1c11c8d4c34d
Naratip Udomsilp <naratip.ud@gmail.com>
2017-05-04 18:24:44 +0700

    :hammer: Revise 'getHeroes, getHero' function & :sparkles: Add 'update' function

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,18 +1,44 @@
+import 'rxjs/add/operator/toPromise';
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 import { Hero } from '../models/hero';
-import { HEROES } from '../models/mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
+  constructor(
+    private http: Http
+  ) { }
 
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: Number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http.put(url, JSON.stringify(hero), { headers: this.headers })
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
 }
